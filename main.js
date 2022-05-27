62707d364ba8450f3c0231c7776965718efb91ee
sanaycode <88113041+sanaycode@users.noreply.github.com>
2022-05-27 11:20:02 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+noseX="";
+noseY="";
+leftWristX="";
+rightWristX="";
+difference="";
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas=createCanvas(550,550);
+    canvas.position(560, 150);
+
+    poseNet= ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function draw(){
+    background('#777d78');
+    document.getElementById("square_size").innerHTML="Width and Height of Square Will be = "+difference+"px";
+    fill('#6603fc');
+    stroke('#6603fc');
+    square(noseX, noseY, difference);
+
+}
+function modelLoaded(){
+    console.log("PoseNet Is Initialized");
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("noseX = "+noseX+"noseY = "+noseY);
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+        difference=floor(leftWristX-rightWristX);
+        console.log("leftWristX = "+leftWristX+"rightWristX = "+rightWristX+"difference = "+difference);
+    }
+}
